d029f17eda20ed064b0d54b6fa7f69ddbcf9859e
matthew yu <matthewyu30121@gmail.com>
2026-09-14 23:56:14 -0400

    fix: totalSizes and some bugs.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Reflection;
 using System.Collections.Immutable;
+using System.Net;
 
 namespace BitTorrent
 {
@@ -146,7 +147,7 @@ namespace BitTorrent
             }
             if (obj.GetType() == typeof(Dictionary<string, object>))
             {
-                EncodeDictionary(buffer, (Dictionary<string, object>obj));
+                EncodeDictionary(buffer, (Dictionary<string, object>)obj);
             }
             else
                 throw new Exception("unable to encode type " + obj.GetType());
@@ -194,7 +195,62 @@ namespace BitTorrent
 
 
     }
+    
 }
+
+
+
+
+
+
+
+    public class Torrent
+    {
+        public  String Name {get;private set;}
+        public bool? IsPrivate {get;private set;}
+        public List<FileItem>Files{get;private set;} = new List<FileItem>();
+        public string FileDirectory {get {return (Files.Count > 1) ? Name + Path.DirectorySeparatorChar:"";}}
+        public List<Tracker> Trackers {get;} = new List<Tracker>();
+        public string Comment {get;set;}
+        public string CreatedBy{get;set;}
+        public DateTime CreationDate{get;set;}
+        public Encoding Encoding{get;set;}
+        public int BlockSize {get; private set;}
+        public int PieceSize{get;private set;}
+        public long TotalSize{get {return Files.Sum(x=>x.Size);}}
+        public byte[][]PieceHashes {get;private set;}
+        public byte[] Infohash{get;set;} = new byte[20];
+        public string HexStringInfohash{get{return String.Join("", this.Infohash.Select(x=>x.ToString("x2")));}}
+        public string UrlSafeStringInfohash {get { return Encoding.UTF8.GetString(WebUtility.UrlEncodeToBytes(this.Infohash, 0,20));}}
+        
+    }
+
+public class FileItem
+{
+    public string Path;
+    public long Size;
+    public long Offset;
+    public string FormattedSize {get {return Torrent.BytesToString(Size);}}
+
+}
+public class Tracker
+{
+    public event EventHandler<List<IPEndPoint>> PeerListUpdated;
+    public string Address {get;private set;}
+    public Tracker(string address)
+    {
+        Address = address;
+    }
+
+}
+
+
+
+
+
+
+
+
 //extend the MemoryStream class to make writing byte arrays a little bit tidier
 public static class MemoryStreamExtensions
 {
